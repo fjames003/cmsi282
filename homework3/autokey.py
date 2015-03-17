f59eae2aed02c5d0548ddff61d18a8e04ddb250b
Frankie <fjames1@lion.lmu.edu>
2015-03-16 23:03:40 -0700

    Finsihed the decoding algorithm for Autokey Vigenere Cipher

--- homework3/autokey.py
+++ homework3/autokey.py
@@ -1,20 +1,24 @@
 def autokey_vigenere_encode(plain, key):
-    plain_points = convert_codepoint(plain.upper())
-    key_points = convert_codepoint(key.upper())
-    cipher_points = []
+    cipher_points, key_points, plain_points = [], convert_codepoint(key.replace(" ", "").upper()), convert_codepoint(plain.replace(" ", "").upper())
     
     for i,c in enumerate(plain_points):
         if i < len(key_points):
             cipher_points.append(65 + ((c + key_points[i]) % 26))
         else:
-            print((c + plain_points[i - len(key_points)]) % 26)
             cipher_points.append(65 + ((c + plain_points[i - len(key_points)]) % 26))
 
-    print cipher_points
     return convert_text(cipher_points)
 
 def autokey_vigenere_decode(cipher, key):
-    pass
+    cipher_points, key_points, plain_points = convert_codepoint(cipher.upper()), convert_codepoint(key.upper()), []
+    
+    for i,c in enumerate(cipher_points):
+        if i < len(key_points):
+            plain_points.append(65 + ((c - key_points[i]) % 26))
+        else:
+            plain_points.append(65 + ((c - plain_points[i - len(key_points)]) % 26))
+
+    return convert_text(plain_points)
 
 def convert_codepoint(text):
     codepoints = [ord(c) for c in text]
@@ -27,7 +31,9 @@ def convert_text(codepoints):
     return text
 
 
-trial = "TAKEACOPYOFYOURPOLICYTONORMAWILCOXONTHETHIRDFLOOR"
-trial = trial.lower()
-print(autokey_vigenere_encode(trial, "quark"))
-print("JUKVKVOZCOHMDSFUMZCTNHZVQPFOJWCOOTWYVVBHUBYHYSWFU")
+trial = "Attack at dawn"
+cipher = autokey_vigenere_encode(trial, "frankie")
+print(cipher)
+plain = autokey_vigenere_decode(cipher, "frankie")
+print(plain)
+
